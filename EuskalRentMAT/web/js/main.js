6fe7254e4ce00934b863c083c48848fc99598670
Asierph <Asierph@192.168.1.133>
2015-11-23 16:22:46 +0100

    Ultima modificación subida por Joseba.

--- EuskalRentMAT/web/js/main.js
+++ EuskalRentMAT/web/js/main.js
@@ -6,15 +6,34 @@ $(document).ready(function(){
     $('.myCarousel').carousel({
         interval: 3000
     });
-    $("#boton").click(function (){ //función para el boton de enviar
+    
+    
+    $("#accede").click(function (){ //función para el boton de enviar
         //recolectamos en variables, lo que tenga cada input.
         //Para mejor manipulación en los if's
-        var nombre = $("#nombre").val();
-        var apellidos = $("#apellidos").val();
-        var correo = $("#correo").val();
-        var passw = $("#pass").val();
-        var repass = $("#repass").val();
+        var nombre = $("#loginEmail").val();
+        var apellidos = $("#loginPass").val();
+
+        
+        $("#menuAccede").remove();
+        $.get("menuUsuario.html", function (data) {
+                    $("#bs-example-navbar-collapse-1").append(data);
+        });
+
+        
+
     });
+    
+    //function to sign in
+    function login() {
+        var inputUsername = localStorage.getItem("username");
+        var inputPassword = localStorage.getItem("password");
+        if (username != "undefined" || username != "null" || password != "undefined" || password !="null")
+        {
+           document.getElementByID('welcomeMessage').innerHTML = "Welcome " + username + "!";
+        } else
+            document.getElementById('welcomeMessage').innerHTML = "Hello!";
+     }
     
     
 });
